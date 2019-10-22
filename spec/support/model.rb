7564a3f1144ae28032787f3fc34b6dba43725445
David <1337dallen@gmail.com>
2019-10-22 17:25:20 +1100

    Add ability to accept options hash
    This required settling BCrypt dependency on ~> 3.1.1 in order to appropriately deal with the cost option

--- spec/support/model.rb
+++ spec/support/model.rb
@@ -5,5 +5,9 @@ class User < Model
 end
 
 class SpecialUser < Model
-  has_protected_token :shared_secret
+  has_protected_token column_name: :shared_secret
+end
+
+class CostedUser < Model
+  has_protected_token cost: 9
 end
